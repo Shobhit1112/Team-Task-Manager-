695b7cde04e5b090b1c8ecb4282b5c1c92d00207
Shobhit Tandon <shobhit@Shobhits-MacBook-Air.local>
2026-05-02 04:06:01 +0530

    Fix vite preview allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['hopeful-unity-production-8b94.up.railway.app'],
+    host: '0.0.0.0',
+    port: 4173
+  }
 })
